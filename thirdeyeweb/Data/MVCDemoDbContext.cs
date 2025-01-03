7e7d25fa90dc889ba65680a7bdfaaa01f50d932c
VENKATESH-33 <avenkat1251@gmail.com>
2025-01-03 18:34:18 +0530

    Add project files.

---
+++ thirdeyeweb/Data/MVCDemoDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using thirdeyeweb.Models.Domain;
+
+namespace thirdeyeweb.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
